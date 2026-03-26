2f38a40c44266b32bc7247176284b01c177d713d
Rifernun <rifernun@gmail.com>
2026-03-26 11:03:44 -0300

    fix(db): correct syntax in Flyway migrations

---
+++ src/main/java/com/richard/inventorymanagement/sector/dto/SectorDto.java
@@ -0,0 +1,4 @@
+package com.richard.inventorymanagement.sector.dto;
+
+public class SectorDto {
+}
---
+++ src/main/java/com/richard/inventorymanagement/sector/entity/Sector.java
@@ -0,0 +1,4 @@
+package com.richard.inventorymanagement.sector.entity;
+
+public class Sector {
+}
